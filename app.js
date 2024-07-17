060883668888ede01bba89dbbcdc0e282ff07192
= <=>
2024-07-17 21:39:37 +0300

    Second

---
+++ app.js
@@ -0,0 +1,15 @@
+let x = 5
+console.log(x)
+
+function close () {
+    let a = 10;
+    console.log(a)
+}
+
+const obj = {
+    name : "Karolis",
+    surname: "seibutis",
+}
+
+obj["name"]= "Spunkinas"
+console.log(obj.name)
